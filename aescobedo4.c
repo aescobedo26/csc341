03a61ee91e640a1ad8bd8565a4814405e5c2f1ff
escoba <aescobedo26@csudh.edu>
2024-09-17 16:59:41 -0700

    Lab #4 code

--- aescobedo4.c
+++ aescobedo4.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//global
 int x;
-int y = 12;
+int y = 15;
 
-//main func
-int main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
-// local
+	int *values;
 	int i;
 	int z = 0;
 
-// array
-	int array[10] = {0};
 
-// pointer
-	int *pointer1;
-//pointer is now equal to the address of varible z
-	pointer1 = &z;
+	values = (int *)malloc(sizeof(int)*5);
 
-	printf("The value of variable z is %d\n",z);
-	printf("The address of z is %p\n", &z);
-	printf("The value of pointer1= %p which is the smae as the address of z which is %p\n", pointer1, &z);
+	printf("Address of argc  is %p\n",&argc);
+	printf("Address of local variable i in stack address is %p\n", &i);
+	printf("Address of the heap variable is %p\n", values);
+	printf("Address for the uninitialized variable x is %p\n", &x);
+	printf("Address for initialized variable y is %p\n", &y);
+	
 
-	for (i = 0;i<10; i++){
-		printf("array[%d]=%d with address of %p\n", i, array[i],&array[i]);
-	}
 	return EXIT_SUCCESS;
 }
